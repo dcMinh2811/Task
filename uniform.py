8e5328f7915383a8763a7a3461df033105c59ac0
dcMinh2811 <dcminh2811@gmail.com>
2025-03-25 23:26:57 +0700

    Add files via upload

--- uniform.py
+++ uniform.py
@@ -1,25 +1,26 @@
-def check_uniform(n):
-    n = str(n)
-    for i in n:
-        if i != n[0]:
-            return False
-    return True
+import time
 
-def are_equal(a):
-    return a
+def measure_runtime(func, *args, **kwargs):
+    start_time = time.perf_counter()
+    result = func(*args, **kwargs)
+    end_time = time.perf_counter()
+    print(f"Runtime: {end_time - start_time:.12f} seconds")
+    return result
 
-def arent_equal(a,b):
-    count = 0
-    for i in range(a,b+1):
-        if check_uniform(i):
-            count += 1
-    print(count)
+def arent_equal(a, b):
+    l = []
+    for n in range(len(str(a)), len(str(b)) + 1):
+        for x in range(1, 10):
+            if a <= int(str(x) * n) <= b:
+                l.append(int(str(x) * n))
+    print(len(l))
+    print(l)
 
 while True:
-    a = int(input("Enter a number(A): "))
-    b = int(input("Enter a number(B): "))
+    a = int(input("Enter a number (A): "))
+    b = int(input("Enter a number (B): "))
     if 0 < a <= b <= 10**12:
         if a == b:
-            are_equal(a)
+            print(1)
         else:
-            arent_equal(a,b)
+            measure_runtime(arent_equal, a, b)
